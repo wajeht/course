a0f8f1b578ca42588d1aa8c1e49c5628db83ce19
wajeht <58354193+wajeht@users.noreply.github.com>
2026-08-11 13:00:39 -0500

    refactor: flatten Vue router module

--- src/vue/main.ts
+++ src/vue/main.ts
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import "./assets/tailwind.css";
-import { router } from "./router";
+import { router } from "./router.js";
 
 createApp(App).use(router).mount("#app");
