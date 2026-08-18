9e3aa2293c858783bef7ff6b554dd7ca973ea221
wajeht <58354193+wajeht@users.noreply.github.com>
2026-08-18 17:29:41 -0500

    refactor: remove routes directory

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import type { ErrorHandler, MiddlewareHandler, NotFoundHandler } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { ZodError } from "zod";
 
-import type { AppEnvironment } from "../configuration.js";
-import type { Logger } from "../logger.js";
+import type { AppEnvironment } from "./configuration.js";
+import type { Logger } from "./logger.js";
 
 export function createMiddleware(
   logger: Logger,
